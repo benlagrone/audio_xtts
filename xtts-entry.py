a489378ac6c5c59c3f7075cc7a7a355143c7f30b
benlagrone <benjaminlagrone@gmail.com>
2025-09-19 17:50:52 -0500

    1

--- xtts-entry.py
+++ xtts-entry.py
@@ -2,8 +2,9 @@
 import sys
 from torch.serialization import add_safe_globals
 from TTS.tts.configs.xtts_config import XttsConfig
+from TTS.tts.models.xtts import XttsAudioConfig
 
-add_safe_globals([XttsConfig])
+add_safe_globals([XttsConfig, XttsAudioConfig])
 
 from TTS.server.server import main
 
