0a4581977e81197bc115661738d2fd1165205e6c
benlagrone <benjaminlagrone@gmail.com>
2025-09-20 15:55:48 -0500

    +!

--- xtts-entry.py
+++ xtts-entry.py
@@ -3,8 +3,9 @@ import sys
 from torch.serialization import add_safe_globals
 from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import XttsAudioConfig
+from TTS.config.shared_configs import BaseDatasetConfig
 
-add_safe_globals([XttsConfig, XttsAudioConfig])
+add_safe_globals([XttsConfig, XttsAudioConfig, BaseDatasetConfig])
 
 from TTS.server.server import main
 
